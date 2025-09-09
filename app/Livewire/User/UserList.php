bf542af217931a378e585fd70bfc4b72c610c277
Dariia Sliusar <dariiasliusar@gmail.com>
2025-09-09 13:04:56 +0200

    refactor user creation logic to use UserForm class and improve validation handling

--- app/Livewire/User/UserList.php
+++ app/Livewire/User/UserList.php
@@ -2,60 +2,18 @@
 
 namespace App\Livewire\User;
 
+use App\Livewire\Forms\UserForm;
 use App\Models\User;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class UserList extends Component
 {
-    #[Validate('required', message: 'Поле обовязкове для заповнення')]
-    #[Validate('min:2', as: 'Імя')]
-    #[Validate('max:25')]
-    public string $name;
-
-    #[Validate('required|email|max:30')]
-    public string $email;
-
-    #[Validate('required|min:6')]
-    public string $password;
-
-    protected function rules(): array
-    {
-        return [
-            'name' => 'required|min:2|max:255',
-            'email' => 'required|email|max:30',
-            'password' => 'required|min:6',
-        ];
-    }
-
-    protected function messages(): array
-    {
-        return [
-            'name.required' => 'Поле обовязкове для заповнення',
-            'name.min' => 'Імя повинно містити мінімум 2 символи',
-            'name.max' => 'Імя повинно містити максимум 255 символів',
-            'email.required' => 'Поле обовязкове для заповнення',
-            'email.email' => 'Введіть коректний email',
-            'email.max' => 'Email повинен містити максимум 30 символів',
-            'password.required' => 'Поле обовязкове для заповнення',
-            'password.min' => 'Пароль повинен містити мінімум 6 символів',
-        ];
-    }
-
+    public UserForm $form;
 
     public function save()
     {
-//        $validated = $this->validate([
-//            'name' => 'required|min:2|max:255',
-//            'email' => 'required|email|max:30',
-//            'password' => 'required|min:6',
-//        ]);
-
-        $validated = $this->validate();
-
-        User::create($validated);
-
-        $this->reset();
+        $this->form->saveUser();
     }
 
     public function delete($id)
