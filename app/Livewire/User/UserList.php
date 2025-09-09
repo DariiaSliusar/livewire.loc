6bf1866660c8f0ef847180e3455b96dd142682f2
Dariia Sliusar <dariiasliusar@gmail.com>
2025-09-09 12:35:37 +0200

    implement form validation and update user creation logic in user list component

--- app/Livewire/User/UserList.php
+++ app/Livewire/User/UserList.php
@@ -3,25 +3,62 @@
 namespace App\Livewire\User;
 
 use App\Models\User;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class UserList extends Component
 {
+    #[Validate('required', message: 'Поле обовязкове для заповнення')]
+    #[Validate('min:2', as: 'Імя')]
+    #[Validate('max:25')]
     public string $name;
+
+    #[Validate('required|email|max:30')]
     public string $email;
+
+    #[Validate('required|min:6')]
     public string $password;
 
-    public function addUser()
+    protected function rules(): array
     {
-        User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => bcrypt($this->password),
-        ]);
-        $this->reset('name', 'email', 'password');
+        return [
+            'name' => 'required|min:2|max:255',
+            'email' => 'required|email|max:30',
+            'password' => 'required|min:6',
+        ];
     }
 
-    public function deleteUser($id)
+    protected function messages(): array
+    {
+        return [
+            'name.required' => 'Поле обовязкове для заповнення',
+            'name.min' => 'Імя повинно містити мінімум 2 символи',
+            'name.max' => 'Імя повинно містити максимум 255 символів',
+            'email.required' => 'Поле обовязкове для заповнення',
+            'email.email' => 'Введіть коректний email',
+            'email.max' => 'Email повинен містити максимум 30 символів',
+            'password.required' => 'Поле обовязкове для заповнення',
+            'password.min' => 'Пароль повинен містити мінімум 6 символів',
+        ];
+    }
+
+
+    public function save()
+    {
+//        $validated = $this->validate([
+//            'name' => 'required|min:2|max:255',
+//            'email' => 'required|email|max:30',
+//            'password' => 'required|min:6',
+//        ]);
+
+        $validated = $this->validate();
+
+        User::create($validated);
+
+        $this->reset();
+    }
+
+    public function delete($id)
     {
         User::query()->find($id)->delete();
     }
